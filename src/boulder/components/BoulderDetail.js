0744b18869ee09092ac30cc1d4244ebc584d1542
Lukas Bruecklmeier <lukas.bruecklmeier@iss-esg.com>
2022-06-07 08:31:38 +0200

    added basic functionality

---
+++ src/boulder/components/BoulderDetail.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {useEffect} from "react";
+
+export default function BoulderDetails(){
+    useEffect(() => {
+        loadBoulder(id)
+    }, [])
+
+    return (<div>Test</div>)
+}
